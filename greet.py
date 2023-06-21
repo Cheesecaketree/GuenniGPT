df439e31dbc098fa4b7ae593d01c139abd423e76
Felix Beermann <felixibbtown@gmail.com>
2023-06-21 15:10:38 +0200

    small correction

--- greet.py
+++ greet.py
@@ -1,5 +1,4 @@
 import openai
-import json
 import os
 
 openai.organization = "org-UmjtjEZDwQcO8rvQHs8tiGKZ"
@@ -9,13 +8,13 @@ def greet(name, channel):
     language = "de"
     time_str = "12:00am"
     
-    system_msg = system_message = {
+    system_msg = {
         "role": "system", "content": "You are a discord bot that greets people joining a voice channel. You get passed name, channel, time and server language. Greet them in a funny and creative way. Keep it short, use the given language, dont use emojis."
     }
     
     messages = [
-        system_message,
-        {"role": "user", "content": f"lang={de}, {name} joined channel \"{channel.name}\" at {time_str}"},
+        system_msg,
+        {"role": "user", "content": f"lang={language}, {name} joined channel \"{channel.name}\" at {time_str}"},
     ]
     
     response = openai.ChatCompletion.create(
