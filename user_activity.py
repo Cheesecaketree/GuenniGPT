54220368dc34ec943b4e1666662bab1574d6ece6
Felix Beermann <felixibbtown@gmail.com>
2023-10-19 01:18:24 +0200

    added possibility to set timeout over config

--- user_activity.py
+++ user_activity.py
@@ -7,9 +7,15 @@ logging.basicConfig(format='%(asctime)s | %(name)s | %(levelname)s | %(message)s
 user_last_join = {}
 
 # check if user was already in channel in the last minute
+import json
+
 def user_recently_joined(channel, member):
+    # get timeout from config.json
+    with open('files/config.json') as f:
+        config = json.load(f)
+    timeout = config['timeout']
     
-    if channel not in user_last_join or member not in user_last_join[channel] or user_last_join[channel][member] + 600 < time.time():
+    if channel not in user_last_join or member not in user_last_join[channel] or user_last_join[channel][member] + timeout < time.time():
         user_last_join[channel] = {}
         user_last_join[channel][member] = time.time()
         return False
