81c9fab61ebaf035ee43733898d218398ba5681a
Felix Beermann <mail@felixbeermann.de>
2024-07-25 23:13:29 +0200

    fixed imports and config access

--- app/modules/service_factory.py
+++ app/modules/service_factory.py
@@ -1,21 +1,22 @@
-from language_models import LanguageModel, OpenAIChatGPT, GroqChat
-from text_to_speech import TextToSpeechEngine, ElevenlabsTTS, OpenAITTS
+from modules.language_models import LanguageModel, OpenAIChatGPT, GroqChat
+from modules.text_to_speech import TextToSpeechEngine, ElevenlabsTTS, OpenAITTS
+
 
 class ServiceFactory:
     def create_language_model(self, config):
-        llm_service = config['LLM']['service']
-        model_name = config['LLM']['model']
+        llm_service = config['llm']['service']
+        model_name = config['llm']['model']
         if llm_service == "OpenAI":
             return OpenAIChatGPT(api_key=config['keys']['openai'], model_name=model_name)
         
         elif llm_service == "Groq":
-            return GroqChat(api_key=config['groq_api_key'], model_name=model_name)
+            return GroqChat(api_key=config['keys']['groq'], model_name=model_name)
     
     def create_text_to_speech_engine(self,config):
-        tts_service = config['TTS']['service']
+        tts_service = config['tts']['service']
         
-        tts_model = config['TTS'][tts_service]['model']
-        tts_voice = config['TTS'][tts_service]['voice']
+        tts_model = config['tts'][tts_service]['model']
+        tts_voice = config['tts'][tts_service]['voice']
         
         if tts_service == "openai":
             return OpenAITTS(api_key=config['keys']['openai'], model=tts_model, voice=tts_voice)
