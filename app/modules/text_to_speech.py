47d8c18b24f2a3b391a3da58974d7cb159dc1328
Felix Beermann <mail@felixbeermann.de>
2024-07-25 23:12:57 +0200

    updated elevenlabs library usage to client approach

--- app/modules/text_to_speech.py
+++ app/modules/text_to_speech.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from openai import OpenAI
-import elevenlabs
-from central_logger import logger
+from elevenlabs.client import ElevenLabs
 import io
 
 # Abstract Class for LLM
@@ -26,7 +25,7 @@ class OpenAITTS(TextToSpeechEngine):
                 input=input
             )
         except Exception as e:
-            logger.error(f"Error using OpenAI TTS: {e}")
+            
             raise e
     
         audio_data = response.content
@@ -36,13 +35,13 @@ class OpenAITTS(TextToSpeechEngine):
 
 class ElevenlabsTTS(TextToSpeechEngine):
     def __init__(self, api_key: str, voice: str, model: str):
-        elevenlabs.set_api_key(api_key=api_key)
         
+        self.client = ElevenLabs(api_key=api_key)
         self.key = api_key
         self.voice = voice
         self.model = model
         
-    def check_quota(self):
+    def check_quota(self, text):
         # check quota
         import requests
         url = "https://api.elevenlabs.io/v1/user/subscription"
@@ -57,19 +56,25 @@ class ElevenlabsTTS(TextToSpeechEngine):
         used = quota_response.json()["character_count"]
         remaining = limit - used
         
-        if remaining < len(self.input):
-            logger.error(f"Elevenlabs TTS quota exceeded: {used}/{limit} characters used")
+        if remaining < len(text):
+            
             raise ValueError(f"Elevenlabs TTS quota exceeded: {used}/{limit} characters used")
         
         
         
     def generate_speech(self, input: str, **kwargs) -> str:
-        self.check_quota()
+        self.check_quota(input)
         
         try:
-            response = elevenlabs.generate(text=input, voice=self.voice, model=self.model)
+            response = self.client.text_to_speech.convert(
+                text=input,
+                output_format="mp3_22050_32",
+                voice_id=self.voice,
+                model_id=self.model,
+            )
+            
         except Exception as e:
-            logger.error(f"Error using Elevenlabs TTS: {e}")
+            
             raise e
     
         audio_stream = io.BytesIO()
