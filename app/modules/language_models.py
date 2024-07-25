5b12aed3aa9c29bf9605f2bc83ed3d284cfc6568
Felix Beermann <mail@felixbeermann.de>
2024-07-26 00:12:43 +0200

    cleanup

--- app/modules/language_models.py
+++ app/modules/language_models.py
@@ -9,7 +9,6 @@ class LanguageModel():
         """Generates text based on given prompt."""
         
     
-    
 class OpenAIChatGPT(LanguageModel):
     def __init__(self, api_key: str, model_name: str = "gpt-4o-mini"):
         self.model = model_name
@@ -26,33 +25,10 @@ class OpenAIChatGPT(LanguageModel):
         
         return response.choices[0].message.content
         
-        
-        
-        
-
 
 # Groq has access to multiple llama models, mixtral 8x7b and google gemma models
 # Price is very low and speed is full insanity
-class GroqChat(LanguageModel):
-    """
-    Tested prompt example for llama-3.1-70b-versatile
-    
-    system:
-You are a Discord Bot that greets people when they enter a voice channel. Only answer in German.
-Stick to the instructions!
-Your purpose is to make the experience in the voice channel more fun. Answer in roughly 4 sentences.
-Today is {Thursday, 25.07.2024}. It is currently {00:44 AM}
-    
-    User:
-    Generate a {STYLE} greeting for "{USERNAME}" who just entered the Discord voice channel "{channelname}".
-    They are currently the only one in the channel. It is 01:23 AM on thursday. Their current discord activity is "{DISCORD ACTIVITY}".
-    
-    tested styles with good results:
-    very formal, sad but funny, sad, sarcastic, formal, confused
-    
-    """
-    
-    
+class GroqChat(LanguageModel): 
     def __init__(self, api_key: str, model_name: str):
         self.client = Groq(api_key=api_key)
         self.model_name = model_name
